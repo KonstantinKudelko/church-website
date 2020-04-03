7f2182ee6fb2fd4067f341ddf4dba0c8d0c12faf
Konstantin Kudelko <konstantinkudelko@mail.ru>
2020-04-03 21:41:53 +0300

    add: populate data before starting server update: README docs

--- server/README.md
+++ server/README.md
@@ -2,13 +2,10 @@
 
 Here user can create articles and other content
 
-# Entities
+# API
 
-The list of available entites, by these you can understand which API call you should use.
-This [link](https://strapi.io/documentation/3.0.0-beta.x/content-api/api-endpoints.html#endpoints) helps you understand format of the requests.
-
-| Entities        | Name           | Table name  |
-| -------------   |:--------------:| -----------:|
-| 1               | Articles       | articles    |
-| 2               | Authors        | authors     |
-| 3               | Tags           | tags        |
+| entity        | request_method | request_url      |
+| ------------- |:--------------:| ----------------:|
+| articles      | GET            | ${host}/articles |
+| authors       | GET            | ${host}/authors  |
+| tags          | GET            | ${host}/tags     |
--- server/config/functions/bootstrap.js
+++ server/config/functions/bootstrap.js
@@ -10,4 +10,26 @@
  * See more details here: https://strapi.io/documentation/3.0.0-beta.x/concepts/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+module.exports = async () => {
+  await strapi.services.tag.create({ title: 'церковь' });
+  await strapi.services.tag.create({ title: 'богословие' });
+
+  await strapi.services.authors.create({
+    first_name: 'Евгений',
+    last_name: 'Бахмутский',
+    facebook_url: 'https://www.facebook.com/bakhmutsky',
+    linkedin_url: 'https://www.facebook.com/bakhmutsky',
+    instagram_url: 'https://www.facebook.com/bakhmutsky',
+    description: 'Pastor pastor pastor',
+    avatar: 'https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-9/82530146_10156531989706008_1129448919923687424_o.jpg?_nc_cat=100&_nc_sid=85a577&_nc_ohc=fSdVVpwLkQEAX_cEoWT&_nc_ht=scontent-frx5-1.xx&oh=d2d0ea9c5a6f70062a128d865472b3fa&oe=5EADBD3D',
+    articles: ['1']
+  })
+
+  await strapi.services.article.create({
+    title: 'Церковь - это жизнь',
+    body: 'Церковь - это жизньЦерковь - это жизньЦерковь - это жизньЦерковь - это жизньЦерковь - это жизньЦерковь - это жизньЦерковь - это жизньЦерковь - это жизньЦерковь - это жизньЦерковь - это жизньЦерковь - это жизнь',
+    cover: 'https://scontent-frx5-1.xx.fbcdn.net/v/t1.0-9/82530146_10156531989706008_1129448919923687424_o.jpg?_nc_cat=100&_nc_sid=85a577&_nc_ohc=fSdVVpwLkQEAX_cEoWT&_nc_ht=scontent-frx5-1.xx&oh=d2d0ea9c5a6f70062a128d865472b3fa&oe=5EADBD3D',
+    tags: ['1', '2'],
+    author: '1'
+  })
+};
