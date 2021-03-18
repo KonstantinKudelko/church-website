23d88d5e9645dad3d32860931b940d9f6bb1410e
Konstantin Kudelko <konstantinkudelko@mail.ru>
2021-03-18 15:25:19 +0200

    feat: remove initial data population

--- server/config/functions/bootstrap.js
+++ server/config/functions/bootstrap.js
@@ -10,30 +10,4 @@
  * See more details here: https://strapi.io/documentation/3.0.0-beta.x/concepts/configurations.html#bootstrap
  */
 
-module.exports = async () => {
-  const { services } = strapi;
-  const authors = await services.author.find();
-
-  if (!authors.length) {
-    await services.tag.create({ title: "церковь" });
-    await services.tag.create({ title: "богословие" });
-
-    await services.author.create({
-      first_name: "Евгений",
-      last_name: "Бахмутский",
-      facebook_url: "https://www.facebook.com/bakhmutsky",
-      linkedin_url: "https://www.facebook.com/bakhmutsky",
-      instagram_url: "https://www.facebook.com/bakhmutsky",
-      description: "Pastor pastor pastor",
-      articles: ["1"],
-    });
-
-    await services.article.create({
-      title: "Церковь - это жизнь",
-      body:
-        "Церковь - это жизньЦерковь - это жизньЦерковь - это жизньЦерковь - это жизньЦерковь - это жизньЦерковь - это жизньЦерковь - это жизньЦерковь - это жизньЦерковь - это жизньЦерковь - это жизньЦерковь - это жизнь",
-      tags: ["1", "2"],
-      author: "1",
-    });
-  }
-};
+module.exports = () => {};
