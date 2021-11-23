a94ec001f1cd50c0ee83c6d1be32bd75945e9f25
Konstantin Kudelko <konstantinkudelko@mail.ru>
2021-11-23 12:41:25 +0300

    feat: first draft of docker files

--- server/config/database.js
+++ server/config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
         client: "postgres",
         host: env("DATABASE_HOST", "localhost"),
         port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapidb"),
+        database: env("DATABASE_NAME", "websitedb"),
         username: env("DATABASE_USERNAME", "root"),
         password: env("DATABASE_PASSWORD", "root"),
         schema: "public",
