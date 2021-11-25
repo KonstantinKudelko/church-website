7a374d99f94251a933052fe7c18295ed05e63144
Konstantin Kudelko <konstantinkudelko@mail.ru>
2021-11-25 11:10:04 +0300

    feat: nginx development client is done

--- client/src/routes.ts
+++ client/src/routes.ts
@@ -1,7 +1,7 @@
-const api = process.env.API_URL || `https://admin.rozhdestvo-youth.by`;
+const api = process.env.API_URL || 'https://admin.rozhdestvo-youth.by';
 
 export const ROUTES = {
   api,
-  articles: `/articles`,
-  notFound: `/404`,
+  articles: '/articles',
+  notFound: '/404',
 } as const;
