435a834bdde74c860aba60b96b8b223803a60961
Konstantin Kudelko <konstantinkudelko@mail.ru>
2021-11-26 15:39:35 +0300

    feat: admin creation for development env

--- server/config/functions/bootstrap.js
+++ server/config/functions/bootstrap.js
@@ -10,4 +10,57 @@
  * See more details here: https://strapi.io/documentation/3.0.0-beta.x/concepts/configurations.html#bootstrap
  */
 
-module.exports = () => {};
+/**
+ * If strapi doesn't have any admins we create
+ * them programmatically.
+ */
+const adminCreation = async () => {
+  const userParams = {
+    username: process.env.DEV_USER || "admin",
+    password: process.env.DEV_PASS || "admin",
+    firstname: process.env.DEV_USER || "Admin",
+    lastname: process.env.DEV_USER || "Admin",
+    email: process.env.DEV_EMAIL || "admin@test.test",
+    blocked: false,
+    isActive: true,
+  };
+  const admins = await strapi.query("user", "admin").find();
+
+  if (!admins.length) {
+    try {
+      const originalPassword = userParams.password;
+      let userRole = await strapi
+        .query("role", "admin")
+        .findOne({ code: "strapi-super-admin" });
+      if (!userRole) {
+        userRole = await strapi.query("role", "admin").create({
+          name: "Super Admin",
+          code: "strapi-super-admin",
+          description:
+            "Super Admins can access and manage all features and settings.",
+        });
+      }
+      userParams.roles = [userRole.id];
+      userParams.password = await strapi.admin.services.auth.hashPassword(
+        originalPassword
+      );
+      await strapi.query("user", "admin").create({
+        ...userParams,
+      });
+      strapi.log.info("Admin account was successfully created.");
+      strapi.log.info(`Email: ${userParams.email}`);
+      strapi.log.info(`Password: ${originalPassword}`);
+    } catch (error) {
+      strapi.log.error(
+        `Couldn't create Admin account during bootstrap: `,
+        error
+      );
+    }
+  }
+};
+
+module.exports = async () => {
+  if (process.env.NODE_ENV === "development") {
+    await adminCreation();
+  }
+};
