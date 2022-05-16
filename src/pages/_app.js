f0356c041f46c12acbf6ef1ceda40c5da7b89fc6
Konstantin Kudelko <konstantinkudelko@mail.ru>
2022-05-16 16:13:47 +0300

    feat: palette, fonts & global css variables

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,3 +1,8 @@
+import "@styles/fonts.css";
+import "@styles/typography.css";
+import "@styles/palette.css";
+import "@styles/global.css";
+
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
