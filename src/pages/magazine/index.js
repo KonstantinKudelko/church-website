0dd9bfb459026757327890871e2759343aeacd83
Konstantin Kudelko <konstantinkudelko@mail.ru>
2022-05-20 17:05:43 +0300

    feat: dirty articles page

--- src/pages/magazine/index.js
+++ src/pages/magazine/index.js
@@ -1,3 +1,33 @@
-export default function Articles() {
-  return <div>It is articles!</div>;
+import fs from "fs";
+import path from "path";
+import parse from "gray-matter";
+
+import { ARTICLES_DIRECTORY } from "@constants";
+
+export default function Articles({ articles }) {
+  return (
+    <div>
+      {articles.map((article) => (
+        <span key={article.slug}>{article.title}</span>
+      ))}
+    </div>
+  );
 }
+
+export const getStaticProps = () => {
+  const paths = fs
+    .readdirSync(ARTICLES_DIRECTORY)
+    .filter((x) => x.endsWith(".mdx"))
+    .map((fileName) => {
+      const articleFilePath = path.join(ARTICLES_DIRECTORY, fileName);
+      const source = fs.readFileSync(articleFilePath);
+      const { data } = parse(source);
+      return data;
+    });
+
+  return {
+    props: {
+      articles: paths,
+    },
+  };
+};
