d85637d32a9614c6e71da49c0d8ec66e89a565f9
ksowy <90048750+ksowy@users.noreply.github.com>
2022-06-23 15:37:21 +0300

    fixed files links

--- src/components/article/components.js
+++ src/components/article/components.js
@@ -1,6 +1,6 @@
-import { Hero } from "@components/article/hero";
+import { Hero } from "@components/hero";
 import { Quote } from "@components/article/quote";
-import { Highlighted } from "@components/article/hero/highlighted";
+import { Highlighted } from "@components/highlighted";
 
 const customComponents = {
   Hero,
