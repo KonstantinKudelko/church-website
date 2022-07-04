6ac9a883f1393c9ce9771684a14985684bf7753f
ksowy <90048750+ksowy@users.noreply.github.com>
2022-07-04 13:29:53 +0300

    feat: start to write validation form code

--- src/components/article/components.js
+++ src/components/article/components.js
@@ -1,9 +1,11 @@
 import { Hero } from "@components/hero";
+import { Form } from "@components/article/form";
 import { Quote } from "@components/article/quote";
 import { Highlighted } from "@components/highlighted";
 
 const customComponents = {
   Hero,
+  Form,
   Quote,
   Highlighted,
 };
--- src/components/article/form/form.js
+++ src/components/article/form/form.js
@@ -1,5 +1,47 @@
+import React, { useState } from "react";
 import styles from "./form.module.css";
 
-export const Form = ({}) => {
-  return <div className={styles.container}></div>;
+export const Form = ({ children }) => {
+  const [value, setValue] = useState({
+    name: "",
+    email: "",
+  });
+
+  const handleChange = (e) => {
+    console.log(e.target.value.trim());
+    setValue({
+      ...value,
+      [e.target.name]: e.target.value.trim(),
+    });
+  };
+
+  const handleSubmit = (e) => {
+    console.log(e);
+    e.preventDefault();
+  };
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.description}>{children}</p>
+      <form className={styles.form}>
+        <input
+          className={styles.name}
+          type="name"
+          placeholder="Имя"
+          value={value.name}
+          onChange={(e) => handleChange(e)}
+        ></input>
+        <input
+          className={styles.email}
+          type="email"
+          placeholder="E-mail"
+          value={value.email}
+          onChange={(e) => handleChange(e)}
+        ></input>
+        <button className={styles.submit} type="submit" onClick={(e) => handleSubmit(e)}>
+          Подписаться &#62;
+        </button>
+      </form>
+    </div>
+  );
 };
