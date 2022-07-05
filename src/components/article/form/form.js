d1c57ed8401e0e1670126728288bd7b7a7b122a5
ksowy <90048750+ksowy@users.noreply.github.com>
2022-07-05 10:48:27 +0300

    feat: add styles for validation form component

--- src/components/article/form/form.js
+++ src/components/article/form/form.js
@@ -1,44 +1,17 @@
-import React, { useState } from "react";
 import styles from "./form.module.css";
 
 export const Form = ({ children }) => {
-  const [value, setValue] = useState({
-    name: "",
-    email: "",
-  });
-
-  const handleChange = (e) => {
-    console.log(e.target.value.trim());
-    setValue({
-      ...value,
-      [e.target.name]: e.target.value.trim(),
-    });
-  };
-
-  const handleSubmit = (e) => {
-    console.log(e);
-    e.preventDefault();
-  };
-
   return (
     <div className={styles.container}>
       <p className={styles.description}>{children}</p>
       <form className={styles.form}>
-        <input
-          className={styles.name}
-          type="name"
-          placeholder="Имя"
-          value={value.name}
-          onChange={(e) => handleChange(e)}
-        ></input>
-        <input
-          className={styles.email}
-          type="email"
-          placeholder="E-mail"
-          value={value.email}
-          onChange={(e) => handleChange(e)}
-        ></input>
-        <button className={styles.submit} type="submit" onClick={(e) => handleSubmit(e)}>
+        <div className={styles.inputContainer}>
+          <input className={styles.name} name="name" type="name" placeholder="Имя" />
+        </div>
+        <div className={styles.inputContainer}>
+          <input className={styles.email} name="email" type="email" placeholder="E-mail" />
+        </div>
+        <button className={styles.submit} type="submit">
           Подписаться &#62;
         </button>
       </form>
