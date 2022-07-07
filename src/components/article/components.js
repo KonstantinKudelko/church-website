118f136775cebe9523a0e933cae0e14a515c3cd9
Vitaly <90048750+ksowy@users.noreply.github.com>
2022-07-07 11:51:06 +0300

    New image components (#60)

--- src/components/article/components.js
+++ src/components/article/components.js
@@ -1,11 +1,15 @@
-import { Hero } from "@components/hero";
-import { Quote } from "@components/article/quote";
-import { Highlighted } from "@components/highlighted";
+import { Hero } from "./hero";
+import { Quote } from "./quote";
+import { Highlighted } from "./highlighted";
+import { LowestImage } from "./lowest-image";
+import { HighestImage } from "./highest-image";
 
 const customComponents = {
   Hero,
   Quote,
   Highlighted,
+  LowestImage,
+  HighestImage,
 };
 
 export const components = {
