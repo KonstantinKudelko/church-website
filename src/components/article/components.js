2da6ae603ebfc18ce6d536a7caaa6c99b8f837b8
Vitaly <90048750+ksowy@users.noreply.github.com>
2022-07-07 16:51:16 +0300

    Thought component (#63)

--- src/components/article/components.js
+++ src/components/article/components.js
@@ -1,17 +1,23 @@
 import { Hero } from "./hero";
 import { Quote } from "./quote";
+import { Thought } from "./thought";
 import { Highlighted } from "./highlighted";
 import { LowestImage } from "./lowest-image";
 import { HighestImage } from "./highest-image";
 import { SubscriptionForm } from "./subscription-form";
+import { ColoredTextBlock } from "./colored-text-block";
+import { HighlightedTextBlock } from "./highlighted-text-block";
 
 const customComponents = {
   Hero,
   Quote,
+  Thought,
   Highlighted,
   LowestImage,
   HighestImage,
   SubscriptionForm,
+  ColoredTextBlock,
+  HighlightedTextBlock,
 };
 
 export const components = {
