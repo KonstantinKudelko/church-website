0a16bb8a23fa272584ad0cfc95b5abe55767012f
Konstantin Kudelko <konstantinkudelko@mail.ru>
2022-08-24 13:34:17 +0300

    feat: subscription form is done

--- src/components/subscription-form/subscription-form.js
+++ src/components/subscription-form/subscription-form.js
@@ -1,11 +1,67 @@
+import { useState } from "react";
+import { Spinner } from "./spinner";
+
 import styles from "./subscription-form.module.css";
 
 export const SubscriptionForm = () => {
-  const onSubmitForm = (event) => {
-    event.preventDefault();
+  const [isLoading, setLoading] = useState(false);
+  const [isSuccess, setSuccess] = useState(false);
 
-    console.log(event.target.name.value, event.target.email.value);
+  const onSubmitForm = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+
+    const token = `Bearer ${localStorage.getItem("token")}`;
+    fetch(`https://api.sendpulse.com/addressbooks/`, {
+      method: "GET",
+      headers: {
+        Authorization: token,
+      },
+    })
+      .then(async (response) => {
+        const json = await response.json();
+        return json[0].id;
+      })
+      .then(async (bookId) => {
+        const body = new FormData();
+        body.append("id", bookId);
+        body.append(
+          "emails",
+          JSON.stringify([
+            {
+              email: event.target.email.value,
+              variables: {
+                name: event.target.name.value,
+              },
+            },
+          ]),
+        );
+        const response = await fetch(`https://api.sendpulse.com/addressbooks/${bookId}/emails`, {
+          method: "POST",
+          body,
+          headers: {
+            Authorization: token,
+          },
+        });
+
+        if (response.ok) {
+          setLoading(false);
+          setSuccess(true);
+        }
+      });
   };
+
+  if (isSuccess) {
+    return (
+      <div className={`${styles.container} ${styles.success}`}>
+        <span className={styles.header}>Поздравляем с подпиской!</span>
+
+        <span className={styles.description}>
+          Мы молимся и верим, что наш ресурс поможет тебе возрастать в христианской вере.
+        </span>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.container}>
@@ -21,12 +77,25 @@ export const SubscriptionForm = () => {
       </span>
 
       <form className={styles.form} onSubmit={onSubmitForm}>
-        <input className={styles.name} name="name" type="name" placeholder="Имя" />
+        <input
+          name="name"
+          type="name"
+          disabled={isLoading}
+          className={styles.name}
+          placeholder="Имя"
+        />
 
-        <input className={styles.email} name="email" type="email" placeholder="E-mail" required />
+        <input
+          name="email"
+          type="email"
+          required
+          disabled={isLoading}
+          className={styles.email}
+          placeholder="E-mail"
+        />
 
-        <button className={styles.submit} type="submit">
-          Подписаться
+        <button className={styles.submit} type="submit" disabled={isLoading}>
+          {!isLoading ? "Подписаться" : <Spinner />}
         </button>
       </form>
     </div>
--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,17 +6,18 @@ import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
   useEffect(() => {
+    const body = new FormData();
+    body.append("grant_type", "client_credentials");
+    body.append("client_id", "6bacec612805a5b964f90e32b66bbad4");
+    body.append("client_secret", "78155d1dbeae51a870ce0af55b70add4");
+
     async function sendPulseAuth() {
       const response = await fetch("https://api.sendpulse.com/oauth/access_token", {
         method: "POST",
-        body: JSON.stringify({
-          grant_type: "client_credentials",
-          client_id: "704f821512042db7e240dfff4c73cb0e",
-          client_secret: "f0c12e1762f6764954329c6fcb2d5578",
-        }),
+        body,
       });
       const data = await response.json();
-      console.log(data);
+      localStorage.setItem("token", data.access_token);
     }
 
     sendPulseAuth();
