b72af5ce76fae7e28e8ac60cb5fa315c80d148b9
Konstantin Kudelko <konstantinkudelko@mail.ru>
2022-09-01 17:41:22 +0300

    fix: gtag constant

--- src/constants/site.js
+++ src/constants/site.js
@@ -1,3 +1,3 @@
-export const GTAG_KEY = "G-90MBGTEX58";
+export const GTAG_KEY = "G-53TJP4XDPX";
 export const ADDRESS_BOOK_ID = 421789;
 export const WEBSITE_BASE_URL = "https://gospel-house.church";
