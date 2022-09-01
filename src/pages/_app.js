62bb982738a2d731fa7bc2cd99de16db9a7105ff
Konstantin Kudelko <konstantinkudelko@mail.ru>
2022-09-01 17:36:39 +0300

    feat: gtag usage

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,9 +3,13 @@ import "@styles/typography.css";
 import "@styles/palette.css";
 import "@styles/global.css";
 import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
   useEffect(() => {
+    // Sendpulse auth
     const body = new FormData();
     body.append("grant_type", "client_credentials");
     body.append("client_id", "6bacec612805a5b964f90e32b66bbad4");
@@ -21,7 +25,18 @@ export default function App({ Component, pageProps }) {
     }
 
     sendPulseAuth();
-  }, []);
+
+    // Gtag logic
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
 
   return <Component {...pageProps} />;
 }
