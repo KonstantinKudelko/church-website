1e92114cfd8dd0dfbf693a59d71fa0eb6c357536
alinaguseva1997 <106080289+alinaguseva1997@users.noreply.github.com>
2023-03-23 13:19:31 +0300

    feat: prayer and the Gospel article (#68)

--- src/views/article/components.js
+++ src/views/article/components.js
@@ -5,6 +5,7 @@ import { Thought } from "@components/thought";
 import { Highlighted } from "@components/highlighted";
 import { LowestImage } from "@components/lowest-image";
 import { HighestImage } from "@components/highest-image";
+import { UnorderedList } from "@components/unordered-list";
 import { SubscriptionForm } from "@components/subscription-form";
 import { ColoredTextBlock } from "@components/colored-text-block";
 import { HighlightedTextBlock } from "@components/highlighted-text-block";
@@ -13,6 +14,7 @@ import { BibleVerse, BiblePassage } from "@components/bible-passage";
 
 const standardComponents = {
   hr: Divider,
+  ul: UnorderedList,
   img: Figure,
 };
 
