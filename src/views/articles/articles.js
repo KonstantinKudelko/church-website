e59bfce9c77e3039b275c5a941a2baca1c91c428
Timosha <flykaiq449@gmail.com>
2023-06-19 15:40:38 +0300

    list layout

--- src/views/articles/articles.js
+++ src/views/articles/articles.js
@@ -1,9 +1,30 @@
+import styles from "./article.module.css";
+
 export const Articles = ({ articles }) => {
   return (
-    <div>
-      {articles.map((article) => (
-        <span key={article.slug}>{article.title}</span>
-      ))}
+    <div className={styles.container}>
+      <hr/>
+      <div className={styles.filter}>
+        <div>Все статьи</div>
+        <div>&#43;</div>
+      </div>
+
+      <div className={styles.list}>
+        {articles.map((article) => (
+          <div key={article.slug}>
+
+            <div className={styles.childData}>
+              <div className={styles.childTitle}>{article.title}</div>
+              <div className={styles.childPoint}>&#x2022;</div>
+              <div className={styles.childAuthor}>{article.author}</div>
+            </div>
+
+            <div className={styles.childTags}>{article.tags.map((tag) => (
+              <div >{tag}</div>
+            ))}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
