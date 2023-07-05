e90aedd8046143916f37d345e15f7b46e36d3075
Konstantin Kudelko <konstantinkudelko@mail.ru>
2023-07-05 11:57:39 +0300

    fix: outside helper issue

--- src/helpers/outside-click.helper.js
+++ src/helpers/outside-click.helper.js
@@ -1,21 +1,21 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect } from "react";
 
 export const useOutsideClick = (callback) => {
   const ref = useRef(null);
-  
+
   useEffect(() => {
     const handleClick = (event) => {
       if (ref.current && !ref.current.contains(event.target)) {
         callback();
-      };
+      }
     };
 
-    document.addEventListener('click', handleClick);
-    
+    document.addEventListener("click", handleClick, true);
+
     return () => {
-      document.removeEventListener('click', handleClick);
+      document.removeEventListener("click", handleClick, true);
     };
-  }, [ref]);
+  }, [ref, callback]);
 
   return ref;
 };
