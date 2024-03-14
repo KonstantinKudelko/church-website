e1505b46116f6ae00aae552b89caa1db310eba5f
alinaguseva1997 <alinagorbunova93@gmail.com>
2024-03-14 14:26:27 +0300

    #71 add a variable in articles

--- src/views/articles/articles.js
+++ src/views/articles/articles.js
@@ -5,7 +5,7 @@ export const Articles = ({articles}) => {
   return (
     <div className={s.container}>
       {articles.map((article) => (
-        <div className={s.article} key={article.slug}>
+        <div className={s.article} key={article.id}>
           <h2>{article.title}</h2>
           <span>•</span>
           <span className={s.author}>{article.author}</span>
