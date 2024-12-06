e57df7dcafae797bf9facf41617bacc1e847a757
Pavel Liolia <pavel.liolia.official@gmail.com>
2024-12-06 18:46:18 +0300

    feat: add logo/home-button to article and main headers

--- src/components/article-header/article-header.js
+++ src/components/article-header/article-header.js
@@ -1,8 +1,13 @@
+import { useRouter } from "next/router";
 import { localizeDateString } from "@helpers/localize-date-string";
+import { Logo } from "@components/logo";
+import { PATH_MAGAZINE } from "@constants";
 
 import styles from "./article-header.module.css";
 
 export const ArticleHeader = ({ src, date, author, children }) => {
+  const router = useRouter();
+
   return (
     <header
       style={{
@@ -10,15 +15,23 @@ export const ArticleHeader = ({ src, date, author, children }) => {
       }}
       className={styles.header}
     >
-      <div className={styles.content}>
-        <span className={styles.author}>{author}</span>
-        {children}
-        <time
-          dateTime={date}
-          className={styles.date}
-        >
-          {localizeDateString(date)}
-        </time>
+      <div
+        className={styles.logo}
+        onClick={() => router.push(PATH_MAGAZINE)}
+      >
+        <Logo />
+      </div>
+      <div className={styles.wrapper}>
+        <div className={styles.content}>
+          <span className={styles.author}>{author}</span>
+          {children}
+          <time
+            dateTime={date}
+            className={styles.date}
+          >
+            {localizeDateString(date)}
+          </time>
+        </div>
       </div>
     </header>
   );
