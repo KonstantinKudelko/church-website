a862e31f870afe0495e3f540984d1de1c9bd233f
Pavel Liolia <pavel.liolia.official@gmail.com>
2024-12-13 21:35:06 +0300

    feat: add breadcrumbs to article header

--- src/components/article-header/article-header.js
+++ src/components/article-header/article-header.js
@@ -1,38 +1,56 @@
-import { useRouter } from "next/router";
+import Link from "next/link";
 import { localizeDateString } from "@helpers/localize-date-string";
 import { Logo } from "@components/logo";
 import { PATH_MAGAZINE } from "@constants";
 
 import styles from "./article-header.module.css";
 
-export const ArticleHeader = ({ src, date, author, children }) => {
-  const router = useRouter();
+const ArticleBreadcrumbs = ({ articleName }) => {
+  const divider = " / ";
 
   return (
-    <header
-      style={{
-        backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${src})`,
-      }}
-      className={styles.header}
-    >
-      <div
-        className={styles.logo}
-        onClick={() => router.push(PATH_MAGAZINE)}
+    <p className={styles.breadcrumbs}>
+      <Link href={PATH_MAGAZINE}>Главная</Link>
+      {divider}
+      <Link href={PATH_MAGAZINE}>Статьи</Link>
+      {articleName && (
+        <>
+          {divider}
+          <span>{articleName}</span>
+        </>
+      )}
+    </p>
+  );
+};
+
+export const ArticleHeader = ({ src, title, date, author, children }) => {
+  return (
+    <>
+      <header
+        style={{
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.3)), url(${src})`,
+        }}
+        className={styles.header}
       >
-        <Logo />
-      </div>
-      <div className={styles.wrapper}>
-        <div className={styles.content}>
-          <span className={styles.author}>{author}</span>
-          {children}
-          <time
-            dateTime={date}
-            className={styles.date}
-          >
-            {localizeDateString(date)}
-          </time>
+        <Link href={PATH_MAGAZINE}>
+          <a className={styles.logo}>
+            <Logo />
+          </a>
+        </Link>
+        <div className={styles.wrapper}>
+          <div className={styles.content}>
+            <span className={styles.author}>{author}</span>
+            {children}
+            <time
+              dateTime={date}
+              className={styles.date}
+            >
+              {localizeDateString(date)}
+            </time>
+          </div>
         </div>
-      </div>
-    </header>
+      </header>
+      <ArticleBreadcrumbs articleName={title} />
+    </>
   );
 };
